9678ab0f82c2697a8389080951fb878861dd9af8
antigame <hoangx321@yahoo.com.vn>
2013-12-30 22:21:57 +0700

    Tạm xong service

---
+++ BestBuy/src/main/java/controller/GetProductInfo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import dao.ProductDAO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pojo.Product;
+
+/**
+ *
+ * @author HOANG
+ */
+@Controller
+@RequestMapping("/product/")
+public class GetProductInfo {
+    @RequestMapping(value="{id}", method = RequestMethod.GET)
+    public @ResponseBody Product getProduct(@PathVariable int id){
+        ApplicationContext context = new ClassPathXmlApplicationContext("hibernate.xml");
+        ProductDAO productDAO = (ProductDAO) context.getBean("productDAO");  
+        Product product = productDAO.get(id);
+        if(product != null){
+            product.setCarts(null);
+            product.setAccessories(null);
+            product.setComments(null);
+            product.setProduct(null);
+            product.setImages(null);
+            product.getCategory().setProducts(null);
+            product.getBrand().setProducts(null);
+        }
+        return product;
+    }
+}
--- BestBuy/src/main/java/controller/GetUserInfo.java
+++ BestBuy/src/main/java/controller/GetUserInfo.java
@@ -1,50 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package controller;
 
 import dao.AccountDAO;
-import javax.servlet.http.HttpServletRequest;
-import org.apache.struts2.interceptor.ServletRequestAware;
-import org.hibernate.Hibernate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pojo.Account;
+
 
 /**
  *
  * @author HOANG
  */
-public class GetUserInfo implements ServletRequestAware {
+@Controller
+@RequestMapping("/user/")
+public class GetUserInfo {
 
-    private HttpServletRequest request;
-    private Account account;
-
-    @Override
-    public void setServletRequest(HttpServletRequest hsr) {
-        request = hsr;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public String execute() {
+    @RequestMapping(value="{name}", method = RequestMethod.GET)
+    public @ResponseBody Account getUser(@PathVariable String name){
         ApplicationContext context = new ClassPathXmlApplicationContext("hibernate.xml");
         AccountDAO accountDAO = (AccountDAO) context.getBean("accountDAO");  
-        account = accountDAO.get(request.getParameter("name"));
-        account.setCarts(null);
-        account.setWishList(null);
-        account.setRoles(null);
-        account.setComments(null);
-        return "success";
+        Account account = accountDAO.get(name);
+        if(account != null){
+            account.setCarts(null);
+            account.setWishList(null);
+            account.setRoles(null);
+            account.setComments(null);
+        }
+        return account;
     }
-    
 }
